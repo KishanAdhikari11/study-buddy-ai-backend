0ec694d6344e1ec422672121c42e124d743c2f6d
kishnadhikari11 <adhikarykishan11@gmail.com>
2025-10-30 08:28:53 +0545

    updated logger

--- utils/logger.py
+++ utils/logger.py
@@ -1,12 +1,27 @@
 import logging
 from datetime import datetime
+from pydantic import BaseModel
 import json
+from contextvars import ContextVar
+
+class RequestContextVar(BaseModel):
+    request_id: str
+    request_path: str
+    
+    
+request_ctx_var: ContextVar[RequestContextVar|None] = ContextVar(
+    "request_ctx_var", default=None
+)
+
 
 class JsonFormatter(logging.Formatter):
     def format(self, record):
+        ctx=request_ctx_var.get()
         log_record={
             "timestamp":datetime.now().isoformat(),
             "level":record.levelname,
+            "request_id": ctx.request_id if ctx else None,
+            "request_path": ctx.request_path if ctx else None,
             "logger":record.name,
             "message":record.getMessage(),
             "filename":record.flename(),
