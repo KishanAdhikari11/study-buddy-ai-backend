ca1e40e6ff8844ca3f352156b8efc2a3da4347e8
kishnadhikari11 <adhikarykishan11@gmail.com>
2025-10-30 08:04:43 +0545

    centralized logging

--- utils/logger.py
+++ utils/logger.py
@@ -1,29 +1,23 @@
 import logging
-import datetime
+from datetime import datetime
 import json
-
-
-
 
 class JsonFormatter(logging.Formatter):
     def format(self, record):
-        log_record = {
-            "timestamp": datetime.utcnow().isoformat(),
-            "level": record.levelname,
-            "logger": record.name,
-            "message": record.getMessage(),
-            "filename": record.filename,
-            "line": record.lineno
+        log_record={
+            "timestamp":datetime.now().isoformat(),
+            "level":record.levelname,
+            "logger":record.name,
+            "message":record.getMessage(),
+            "filename":record.flename(),
+            "line":record.lineno
         }
         return json.dumps(log_record)
     
-def get_logger(name):
-    logger = logging.getLogger(name)
+    
+def get_logger(name:str):
+    logger=logging.getLogger(name)
     logger.setLevel(logging.INFO)
-
-    handler = logging.FileHandler(f"{name}.log")
+    handler= logging.FileHandler(f"{name}.log")
     handler.setFormatter(JsonFormatter())
-
-    logger.addHandler(handler)
-
     return logger
