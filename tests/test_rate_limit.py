685b21168bce9908162cf85fa088303991df172f
Kishan Adhikari <61057393+KishanAdhikari11@users.noreply.github.com>
2026-04-18 20:47:22 +0545

    Chatbot implementation (#32)
    * fix: formatter error
    
    * nitpick : reduce rate limit

--- tests/test_rate_limit.py
+++ tests/test_rate_limit.py
@@ -6,7 +6,7 @@ client = TestClient(app)
 
 
 def test_rate_limit():
-    for i in range(5):
+    for i in range(50):
         response = client.get("/")
         assert response.status_code == 200, f"Failed at {i + 1}"
     response = client.get("/")
